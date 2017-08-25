37edfcf1c5b71076f2d68e0f7e3e8865c35b1d47
marc <19379@users.noreply.github.com>
2017-08-25 10:39:20 +0100

    samples: add on/off toggle to listenbrainz

--- foo_jscript_panel/samples/complete/js/listenbrainz.js
+++ foo_jscript_panel/samples/complete/js/listenbrainz.js
@@ -9,7 +9,7 @@ _.mixin({
 		
 		this.playback_time = function () {
 			this.time_elapsed++;
-			if (this.time_elapsed == this.target_time)
+			if (this.enabled && this.time_elapsed == this.target_time)
 				this.listen(this.metadb);
 		}
 		
@@ -25,117 +25,119 @@ _.mixin({
 			if (!tags.artist || !tags.title)
 				return console.log('Artist/title tag missing. Not submitting.');
 			
-			var data = {
-				listen_type : 'single',
-				payload : [
-					{
-						listened_at : this.timestamp,
-						track_metadata : {
-							additional_info : {
-								// must be arrays
-								artist_mbids : tags.musicbrainz_artistid,
-								tags : _.map(_.take(tags.genre, 50), function (item) { return item.substring(0, 64); }), // as per API DOCS!!
-								work_mbids : tags.musicbrainz_workid,
-								// must be strings
-								albumartist : _.first(tags.albumartist),
-								date : _.first(tags.date),
-								discnumber : _.first(tags.discnumber),
-								isrc : _.first(tags.isrc),
-								recording_mbid : _.first(tags.musicbrainz_trackid),
-								release_group_mbid : _.first(tags.musicbrainz_releasegroupid),
-								release_mbid : _.first(tags.musicbrainz_albumid),
-								totaldiscs : _.first(tags.totaldiscs),
-								totaltracks: _.first(tags.totaltracks),
-								track_mbid : _.first(tags.musicbrainz_releasetrackid),
-								tracknumber : _.first(tags.tracknumber)
-							},
-							artist_name : _.first(tags.artist),
-							release_name : _.first(tags.album),
-							track_name : _.first(tags.title)
-						}
-					}
-				]
+			var payload = {
+				listened_at : this.timestamp,
+				track_metadata : {
+					additional_info : {
+						// must be arrays
+						artist_mbids : tags.musicbrainz_artistid,
+						work_mbids : tags.musicbrainz_workid,
+						// must be strings
+						albumartist : _.first(tags.albumartist),
+						date : _.first(tags.date),
+						discnumber : _.first(tags.discnumber),
+						isrc : _.first(tags.isrc),
+						recording_mbid : _.first(tags.musicbrainz_trackid),
+						release_group_mbid : _.first(tags.musicbrainz_releasegroupid),
+						release_mbid : _.first(tags.musicbrainz_albumid),
+						totaldiscs : _.first(tags.totaldiscs),
+						totaltracks: _.first(tags.totaltracks),
+						track_mbid : _.first(tags.musicbrainz_releasetrackid),
+						tracknumber : _.first(tags.tracknumber)
+					},
+					artist_name : _.first(tags.artist),
+					release_name : _.first(tags.album),
+					track_name : _.first(tags.title)
+				}
 			};
 			
+			if (this.submit_genres && tags.genre)
+				payload.track_metadata.additional_info.tags = _(tags.genre)
+					.take(50)
+					.map(function (item) {
+						return item.substring(0, 64);
+					})
+					.value();
+					
 			console.log('Submitting ' + _.q(tags.artist + ' - ' + tags.title));
 			
 			if (this.show_data)
-				fb.Trace(JSON.stringify(data, null, 4));
+				fb.Trace(JSON.stringify(payload, null, 4));
 			
-			this.post(data);
-		}
-		
-		this.post = function (data) {
-			this.xmlhttp.open('POST', 'https://api.listenbrainz.org/1/submit-listens', true);
-			this.xmlhttp.setRequestHeader('Authorization' , 'Token ' + this.token);
-			this.xmlhttp.send(JSON.stringify(data));
-			this.xmlhttp.onreadystatechange = _.bind(function () {
-				if (this.xmlhttp.readyState == 4) {
-					if (this.xmlhttp.responseText) {
-						var response = _.jsonParse(this.xmlhttp.responseText);
-						if (response.status == 'ok') {
-							console.log('Listen submitted OK!');
-							// now would be a good time to retry any listens in the cache
-							if (this.open_cache().length)
-								this.retry();
-						} else if (response.code && response.error) {
-							console.log('Error code: ' + response.code);
-							console.log('Error message: ' + response.error);
-							if (response.code == 400) {
-								console.log('Not caching listen with a 400 response as it is malformed and will get rejected again. See note in main script about reporting errors.');
-							} else {
-								this.cache(data);
-							}
-						} else {
-							console.log(this.xmlhttp.responseText);
-							this.cache(data);
-						}
-					} else {
-						console.log('The server response was empty, status code: ' + this.xmlhttp.status);
-						if (this.xmlhttp.status == 0)
-							console.log('A possible cause of this may be an invalid authorization token.');
-						this.cache(data);
-					}
-				}
-			}, this);
+			this.post('single', [payload]);
 		}
 		
 		this.retry = function () {
 			if (this.cache_is_bad)
 				return;
-			var tmp = this.open_cache();
+			var payload = _.take(this.open_cache(), this.max_listens);
+			this.post('import', payload);
+		}
+		
+		this.post = function (listen_type, payload) {
 			var data = {
-				listen_type : 'import',
-				payload : _.take(tmp, this.max_listens)
+				listen_type : listen_type,
+				payload : payload
 			}
 			this.xmlhttp.open('POST', 'https://api.listenbrainz.org/1/submit-listens', true);
 			this.xmlhttp.setRequestHeader('Authorization' , 'Token ' + this.token);
 			this.xmlhttp.send(JSON.stringify(data));
 			this.xmlhttp.onreadystatechange = _.bind(function () {
 				if (this.xmlhttp.readyState == 4) {
-					if (this.xmlhttp.responseText) {
-						var response = _.jsonParse(this.xmlhttp.responseText);
-						if (response.status == 'ok') {
-							console.log(data.payload.length + ' cached listens submitted OK!');
-							_.save(JSON.stringify(_.drop(this.open_cache(), this.max_listens)), this.cache_file);
-							if (this.open_cache().length) {
-								window.SetTimeout(_.bind(function () {
+					switch (listen_type) {
+					case 'single':
+						if (this.xmlhttp.responseText) {
+							var response = _.jsonParse(this.xmlhttp.responseText);
+							if (response.status == 'ok') {
+								console.log('Listen submitted OK!');
+								// now would be a good time to retry any listens in the cache
+								if (this.open_cache().length)
 									this.retry();
-								}, this), 1000);
+							} else if (response.code && response.error) {
+								console.log('Error code: ' + response.code);
+								console.log('Error message: ' + response.error);
+								if (response.code == 400) {
+									console.log('Not caching listen with a 400 response as it is malformed and will get rejected again. See note in main script about reporting errors.');
+								} else {
+									this.cache(data);
+								}
 							} else {
-								console.log('Cache is now clear!');
+								console.log(this.xmlhttp.responseText);
+								this.cache(data);
 							}
-						} else if (response.code == 400) {
-							if (response.error)
-								console.log(response.error);
-							console.log('Cannot retry submitting cache until bad entry is fixed/removed. See note in main script about reporting errors.')
-							this.cache_is_bad = true;
+						} else {
+							console.log('The server response was empty, status code: ' + this.xmlhttp.status);
+							if (this.xmlhttp.status == 0)
+								console.log('A possible cause of this may be an invalid authorization token.');
+							this.cache(data);
 						}
+						break;
+					case 'import':
+						if (this.xmlhttp.responseText) {
+							var response = _.jsonParse(this.xmlhttp.responseText);
+							if (response.status == 'ok') {
+								console.log(data.payload.length + ' cached listen(s) submitted OK!');
+								_.save(JSON.stringify(_.drop(this.open_cache(), this.max_listens)), this.cache_file);
+								if (this.open_cache().length) {
+									window.SetTimeout(_.bind(function () {
+										this.retry();
+									}, this), 1000);
+								} else {
+									console.log('Cache is now clear!');
+								}
+							} else if (response.code == 400) {
+								if (response.error)
+									console.log(response.error);
+								console.log('Cannot retry submitting cache until bad entry is fixed/removed. See note in main script about reporting errors.')
+								this.cache_is_bad = true;
+							}
+						}
+						break;
 					}
 				}
 			}, this);
 		}
-
+		
 		this.cache = function (data) {
 			var tmp = this.open_cache();
 			tmp.unshift(data.payload[0]);
@@ -152,11 +154,7 @@ _.mixin({
 			var f = metadb.GetFileInfo();
 			for (var i = 0; i < f.MetaCount; i++) {
 				var name = f.MetaName(i).toLowerCase();
-				if (name == 'genre' && !this.submit_genres)
-					continue;
-				
 				var key = this.mapping[name] || name;
-				
 				tmp[key] = [];
 				for (var j = 0; j < f.MetaValueCount(i); j++) {
 					var value = f.MetaValue(i, j);
@@ -182,14 +180,17 @@ _.mixin({
 			m.AppendMenuItem(MF_STRING, 2, 'Set username...');
 			m.AppendMenuItem(this.username.length ? MF_STRING : MF_GRAYED, 3, 'View profile');
 			m.AppendMenuSeparator();
-			m.AppendMenuItem(flag, 4, 'Show submission data in Console when sending');
-			m.CheckMenuItem(4, this.show_data);
-			m.AppendMenuItem(flag, 5, 'Submit Media Library tracks only');
-			m.CheckMenuItem(5, this.in_library);
-			m.AppendMenuItem(flag, 6, 'Submit genre tags');
-			m.CheckMenuItem(6, this.submit_genres);
+			m.AppendMenuItem(flag, 4, 'Enabled');
+			m.CheckMenuItem(4, this.enabled);
 			m.AppendMenuSeparator();
-			m.AppendMenuItem(MF_GRAYED, 7, 'Cache contains ' + this.open_cache().length + ' listen(s).');
+			m.AppendMenuItem(flag, 5, 'Show submission data in Console when sending');
+			m.CheckMenuItem(5, this.show_data);
+			m.AppendMenuItem(flag, 6, 'Submit Media Library tracks only');
+			m.CheckMenuItem(6, this.in_library);
+			m.AppendMenuItem(flag, 7, 'Submit genre tags');
+			m.CheckMenuItem(7, this.submit_genres);
+			m.AppendMenuSeparator();
+			m.AppendMenuItem(MF_GRAYED, 8, 'Cache contains ' + this.open_cache().length + ' listen(s).');
 			var idx = m.TrackPopupMenu(this.x, this.y + this.size);
 			switch (idx) {
 			case 1:
@@ -211,14 +212,19 @@ _.mixin({
 				_.run('https://listenbrainz.org/user/' + this.username);
 				break;
 			case 4:
+				this.enabled = !this.enabled;
+				window.SetProperty('2K3.LISTENBRAINZ.ENABLED', this.enabled);
+				this.update_button();
+				break;
+			case 5:
 				this.show_data = !this.show_data;
 				window.SetProperty('2K3.LISTENBRAINZ.SHOW.DATA', this.show_data);
 				break;
-			case 5:
+			case 6:
 				this.in_library = !this.in_library;
 				window.SetProperty('2K3.LISTENBRAINZ.IN.LIBRARY', this.in_library);
 				break;
-			case 6:
+			case 7:
 				this.submit_genres = !this.submit_genres;
 				window.SetProperty('2K3.LISTENBRAINZ.SUBMIT.GENRES', this.submit_genres);
 				break;
@@ -227,7 +233,7 @@ _.mixin({
 		}
 		
 		this.update_button = function () {
-			buttons.buttons.listenbrainz = new _.button(this.x, this.y, this.size, this.size, {normal : _.isUUID(this.token) ? 'misc\\listenbrainz_active.png' : 'misc\\listenbrainz_inactive.png'}, _.bind(function () { this.options(); }, this), 'Listenbrainz Options');
+			buttons.buttons.listenbrainz = new _.button(this.x, this.y, this.size, this.size, {normal : this.enabled && _.isUUID(this.token) ? 'misc\\listenbrainz_active.png' : 'misc\\listenbrainz_inactive.png'}, _.bind(function () { this.options(); }, this), 'Listenbrainz Options');
 			window.RepaintRect(this.x, this.y, this.size, this.size);
 		}
 		
@@ -244,6 +250,7 @@ _.mixin({
 		this.show_data = window.GetProperty('2K3.LISTENBRAINZ.SHOW.DATA', false);
 		this.in_library = window.GetProperty('2K3.LISTENBRAINZ.IN.LIBRARY', false);
 		this.submit_genres = window.GetProperty('2K3.LISTENBRAINZ.SUBMIT.GENRES', true);
+		this.enabled = window.GetProperty('2K3.LISTENBRAINZ.ENABLED', true);
 		this.xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
 		this.time_elapsed = 0;
 		this.target_time = 0;
