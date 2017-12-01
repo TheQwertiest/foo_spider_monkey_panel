c7057db58b4a064503c3f0e320e87b56eb4dc219
marc <19379@users.noreply.github.com>
2017-12-01 04:20:45 +0000

    Tidy up resource files

--- src/resource.h
+++ src/resource.h
@@ -6,50 +6,50 @@
 #define IDC_STATIC -1
 #endif
 
-#define IDD_DIALOG_CONFIG               101
-#define IDR_SCRIPT                      102
+#define IDR_SCRIPT                      101
+#define IDD_DIALOG_CONFIG               102
 #define IDD_DIALOG_FIND                 103
 #define IDD_DIALOG_GOTO                 104
 #define IDD_DIALOG_REPLACE              105
 #define IDD_DIALOG_PREFERENCE           107
 #define IDD_DIALOG_NAME_VALUE           108
 #define IDD_DIALOG_PROPERTIES           109
-#define IDC_CHECK_PSEUDO_TRANSPARENT    1001
-#define IDC_EXPORT                      1002
-#define IDC_EDIT_LINENUMBER             1003
-#define IDC_FINDDOWN                    1004
-#define IDC_FINDUP                      1005
-#define IDC_EDIT_FINDWHAT               1006
-#define IDC_CHECK_MATCHCASE             1007
-#define IDC_CHECK_WHOLEWORD             1008
-#define IDC_CHECK_WORDSTART             1009
-#define IDC_CHECK_REGEXP                1010
-#define IDC_EDIT_REPLACE                1011
-#define IDC_FINDNEXT                    1012
-#define IDC_REPLACE                     1013
-#define IDC_REPLACEALL                  1014
-#define IDC_CHECK_GRABFOCUS             1015
-#define IDC_LIST_EDITOR_PROP            1016
-#define IDC_EDIT_VALUE                  1017
-#define IDC_EDIT_NAME                   1018
-#define IDC_BUTTON_APPLY                1019
-#define IDC_BUTTON_IMPORT               1020
-#define IDC_BUTTON_EXPORT               1021
-#define IDC_LIST_PROPERTIES             1022
-#define IDC_STATIC_EDGE                 1023
-#define IDC_EDIT                        1024
-#define IDC_IMPORT                      1027
-#define IDAPPLY                         1028
-#define IDC_CLEARALL                    1029
-#define IDC_DEL                         1030
-#define IDC_EDIT_TIMEOUT                1031
-#define IDC_SPIN_TIMEOUT                1032
-#define IDC_EDGE_STYLE                  1034
-#define IDC_STATIC_GUID                 1035
-#define IDC_TOOLS                       1036
-#define IDC_CHECK1                      1038
-#define IDC_CHECK_DEBUG_MODE            1038
-#define IDC_EDIT1                       1039
+#define IDAPPLY                         110
+
+#define IDC_STATIC_GUID                 1000
+#define IDC_EDIT                        1001
+#define IDC_TOOLS                       1002
+#define IDC_STATIC_EDGE                 1003
+#define IDC_EDGE_STYLE                  1004
+#define IDC_CHECK_PSEUDO_TRANSPARENT    1005
+#define IDC_CHECK_GRABFOCUS             1006
+
+#define IDC_DEL                         1010
+#define IDC_CLEARALL                    1011
+#define IDC_IMPORT                      1012
+#define IDC_EXPORT                      1013
+#define IDC_LIST_PROPERTIES             1014
+
+#define IDC_EDIT_FINDWHAT               1020
+#define IDC_EDIT_REPLACE                1021
+#define IDC_CHECK_MATCHCASE             1022
+#define IDC_CHECK_WHOLEWORD             1023
+#define IDC_CHECK_WORDSTART             1024
+#define IDC_CHECK_REGEXP                1025
+#define IDC_FINDDOWN                    1026
+#define IDC_FINDUP                      1027
+#define IDC_FINDNEXT                    1028
+#define IDC_REPLACE                     1029
+#define IDC_REPLACEALL                  1030
+
+#define IDC_EDIT_LINENUMBER             1040
+
+#define IDC_LIST_EDITOR_PROP            1050
+#define IDC_BUTTON_IMPORT               1051
+#define IDC_BUTTON_EXPORT               1052
+
+#define IDC_EDIT_NAME                   1060
+#define IDC_EDIT_VALUE                  1061
 
 // Next default values for new objects
 #ifdef APSTUDIO_INVOKED
