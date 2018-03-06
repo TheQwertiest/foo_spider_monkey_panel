9990ed17cfae193076523284a4b7ce505e8e53e3
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2018-03-06 23:00:49 +0000

    Some panel manager shenanigans

--- src/panel_manager.h
+++ src/panel_manager.h
@@ -99,26 +99,23 @@ private:
 	PFC_CLASS_NOT_COPYABLE_EX(panel_manager)
 };
 
-class nonautoregister_callbacks : public initquit, public metadb_io_callback_dynamic, public ui_selection_callback, public replaygain_core_settings_notify
+class my_initquit : public initquit, public ui_selection_callback, public replaygain_core_settings_notify
 {
 public:
 	virtual void on_init()
 	{
-		metadb_io_v3::get()->register_callback(this);
 		replaygain_manager_v2::get()->add_notify(this);
 		ui_selection_manager_v2::get()->register_callback(this, 0);
 	}
 
 	virtual void on_quit()
 	{
-		ui_selection_manager_v2::get()->unregister_callback(this);
 		replaygain_manager_v2::get()->remove_notify(this);
-		metadb_io_v3::get()->unregister_callback(this);
+		ui_selection_manager_v2::get()->unregister_callback(this);
 	}
 
-	virtual void on_changed_sorted(metadb_handle_list_cref p_items_sorted, bool p_fromhook);
-	virtual void on_selection_changed(metadb_handle_list_cref p_selection);
 	virtual void on_changed(t_replaygain_config const& cfg);
+	virtual void on_selection_changed(metadb_handle_list_cref p_selection);
 };
 
 class my_library_callback : public library_callback
@@ -127,6 +124,12 @@ public:
 	virtual void on_items_added(metadb_handle_list_cref p_data);
 	virtual void on_items_modified(metadb_handle_list_cref p_data);
 	virtual void on_items_removed(metadb_handle_list_cref p_data);
+};
+
+class my_metadb_io_callback : public metadb_io_callback
+{
+public:
+	virtual void on_changed_sorted(metadb_handle_list_cref p_items_sorted, bool p_fromhook);
 };
 
 class my_play_callback_static : public play_callback_static
