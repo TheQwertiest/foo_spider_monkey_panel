e91a76d7cf1c4bc46d214b67a903d75db1be616c
TheQwertiest <qwertiest@mail.ru>
2018-07-25 17:42:23 +0300

    Removed JS engine settings form panel

--- foo_spider_monkey_panel/resource.h
+++ foo_spider_monkey_panel/resource.h
@@ -1,11 +1,7 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
+// Used by foo_spider_monkey_panel.rc
 //
-
-#ifndef IDC_STATIC
-#define IDC_STATIC -1
-#endif
-
 #define IDR_SCRIPT                      101
 #define IDD_DIALOG_CONFIG               102
 #define IDD_DIALOG_FIND                 103
@@ -15,7 +11,6 @@
 #define IDD_DIALOG_NAME_VALUE           108
 #define IDD_DIALOG_PROPERTIES           109
 #define IDAPPLY                         110
-
 #define IDC_STATIC_GUID                 1000
 #define IDC_EDIT                        1001
 #define IDC_TOOLS                       1002
@@ -25,13 +20,11 @@
 #define IDC_COMBO_EDGE                  1006
 #define IDC_CHECK_PSEUDO_TRANSPARENT    1007
 #define IDC_CHECK_GRABFOCUS             1008
-
 #define IDC_DEL                         1010
 #define IDC_CLEARALL                    1011
 #define IDC_IMPORT                      1012
 #define IDC_EXPORT                      1013
 #define IDC_LIST_PROPERTIES             1014
-
 #define IDC_EDIT_FINDWHAT               1020
 #define IDC_EDIT_REPLACE                1021
 #define IDC_CHECK_MATCHCASE             1022
@@ -43,20 +36,19 @@
 #define IDC_FINDNEXT                    1028
 #define IDC_REPLACE                     1029
 #define IDC_REPLACEALL                  1030
-
 #define IDC_EDIT_LINENUMBER             1040
-
 #define IDC_LIST_EDITOR_PROP            1050
 #define IDC_BUTTON_IMPORT               1051
 #define IDC_BUTTON_EXPORT               1052
-
 #define IDC_EDIT_NAME                   1060
 #define IDC_EDIT_VALUE                  1061
+#define IDC_STATIC                      -1
 
 // Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_RESOURCE_VALUE        111
 #define _APS_NEXT_COMMAND_VALUE         40012
 #define _APS_NEXT_CONTROL_VALUE         1040
 #define _APS_NEXT_SYMED_VALUE           101
