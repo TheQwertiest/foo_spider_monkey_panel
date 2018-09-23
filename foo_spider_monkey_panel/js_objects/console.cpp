0bb43f50e8a39da4124eab1d5ea7f1eae10847fd
TheQwertiest <qwertiest@mail.ru>
2018-09-23 20:08:49 +0300

    Fixed uncaught JS exception in `console.log`

--- foo_spider_monkey_panel/js_objects/console.cpp
+++ foo_spider_monkey_panel/js_objects/console.cpp
@@ -4,55 +4,57 @@
 #include <js_engine/js_to_native_invoker.h>
 #include <js_utils/js_object_helper.h>
 
-
 namespace
 {
 
 using namespace mozjs;
 
 // TODO: wrap in a proper class
-// TODO: add printf-like formating as required by W3C 
+// TODO: add printf-like formating as required by W3C
 bool LogImpl( JSContext* cx, unsigned argc, JS::Value* vp )
 {
-     JS::CallArgs args = JS::CallArgsFromVp( argc, vp );
+    JS::CallArgs args = JS::CallArgsFromVp( argc, vp );
 
-     pfc::string8_fast outputString;
+    pfc::string8_fast outputString;
 
-     for ( unsigned i = 0; i < args.length(); i++ )
-     {
-         auto retVal = convert::to_native::ToValue<pfc::string8_fast>( cx, args[i] );
-         pfc::string8_fast parsedArg = retVal ? retVal.value() : "__parsing_failed__";
+    for ( unsigned i = 0; i < args.length(); i++ )
+    {
+        auto retVal = convert::to_native::ToValue<pfc::string8_fast>( cx, args[i] );
+        if ( !retVal )
+        {
+            return false;
+        }
 
-         outputString += parsedArg;
-         if ( i < args.length() )
-         {
-             outputString.add_char( ' ' );
-         }
-     }
+        outputString += retVal.value();
+        if ( i < args.length() )
+        {
+            outputString.add_char( ' ' );
+        }
+    }
 
-     args.rval().setUndefined();
-        
-     console::info( outputString.c_str() );
-     return true;
+    args.rval().setUndefined();
+
+    console::info( outputString.c_str() );
+    return true;
 }
 
-MJS_WRAP_JS_TO_NATIVE_FN(Log, LogImpl)
+MJS_WRAP_JS_TO_NATIVE_FN( Log, LogImpl )
 
 static const JSFunctionSpec console_functions[] = {
-     JS_FN( "log", Log, 0, DefaultPropsFlags() ),
-     JS_FS_END
+    JS_FN( "log", Log, 0, DefaultPropsFlags() ),
+    JS_FS_END
 };
-}
+} // namespace
 
 namespace mozjs
 {
 
 bool DefineConsole( JSContext* cx, JS::HandleObject global )
 {
-     JS::RootedObject consoleObj( cx, JS_NewPlainObject( cx ) );
-     return consoleObj
-          && JS_DefineFunctions( cx, consoleObj, console_functions )
-          && JS_DefineProperty( cx, global, "console", consoleObj, DefaultPropsFlags() );
+    JS::RootedObject consoleObj( cx, JS_NewPlainObject( cx ) );
+    return consoleObj
+           && JS_DefineFunctions( cx, consoleObj, console_functions )
+           && JS_DefineProperty( cx, global, "console", consoleObj, DefaultPropsFlags() );
 }
 
-}
+} // namespace mozjs
