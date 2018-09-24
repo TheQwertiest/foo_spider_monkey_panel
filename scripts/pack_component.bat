139797a2c2016de1378d5dbea5cf20f96d411ed1
TheQwertiest <qwertiest@mail.ru>
2018-09-24 23:49:42 +0300

    Added .pdb to mozjs archive + updated pack script

--- scripts/pack_component.bat
+++ scripts/pack_component.bat
@@ -10,7 +10,7 @@ if '%1'=='--debug' (
 
 set COMPONENT_DIR_NO_SLASH=%ROOT_DIR%component
 set COMPONENT_DLL=%ROOT_DIR%_result\Win32_%CONFIGURATION%\bin\foo_spider_monkey_panel.dll
-set MOZ_JS_BIN_DIR_NO_SLASH=%ROOT_DIR%mozjs\%CONFIGURATION%\bin
+set MOZ_JS_BIN_DIR=%ROOT_DIR%mozjs\%CONFIGURATION%\bin\
 set COMPONENT_OUT_DIR_NO_SLASH=%ROOT_DIR%_result\Win32_%CONFIGURATION%\component
 set COMPONENT_OUT_DIR=%COMPONENT_OUT_DIR_NO_SLASH%\
 set FB2K_ARCHIVE=%ROOT_DIR%foo_spider_monkey_panel.fb2k-component
@@ -19,7 +19,7 @@ set FB2K_ARCHIVE=%ROOT_DIR%foo_spider_monkey_panel.fb2k-component
 
 if not exist "%COMPONENT_OUT_DIR_NO_SLASH%" mkdir "%COMPONENT_OUT_DIR_NO_SLASH%"
 xcopy /r/y/s "%COMPONENT_DIR_NO_SLASH%" "%COMPONENT_OUT_DIR_NO_SLASH%"
-xcopy /r/y/s "%MOZ_JS_BIN_DIR_NO_SLASH%" "%COMPONENT_OUT_DIR_NO_SLASH%"
+xcopy /r/y/s "%MOZ_JS_BIN_DIR_NO_SLASH%*.dll" "%COMPONENT_OUT_DIR%"
 xcopy /r/y "%COMPONENT_DLL%" "%COMPONENT_OUT_DIR%"
 
 if exist "%FB2K_ARCHIVE%" del /f/q "%FB2K_ARCHIVE%"
