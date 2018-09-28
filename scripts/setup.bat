5b5535bf9bae1fe665338663f5bdf112b46e131d
TheQwertiest <qwertiest@mail.ru>
2018-09-28 13:55:28 +0300

    Fixed path in scripts

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 
+set CUR_DIR=%~dp0
 set ROOT_DIR=%~dp0..\
 if not '%1'=='--skip_mozjs' (
     set ROOT_DIR=%1
@@ -16,14 +17,14 @@ if '%2'=='--skip_mozjs' (
 
 echo Preparing project repo
 
-call fetch_submodules.bat %ROOT_DIR%
+call %CUR_DIR%fetch_submodules.bat %ROOT_DIR%
 if errorlevel 1 goto fail
-call prepare_scintilla.bat %ROOT_DIR%
+call %CUR_DIR%prepare_scintilla.bat %ROOT_DIR%
 if errorlevel 1 goto fail
-call patch_submodules.bat %ROOT_DIR%
+call %CUR_DIR%patch_submodules.bat %ROOT_DIR%
 if errorlevel 1 goto fail
 if '%SKIP_MOZJS%'=='' (
-    call unpack_mozjs.bat %ROOT_DIR%
+    call %CUR_DIR%unpack_mozjs.bat %ROOT_DIR%
     if errorlevel 1 goto fail
 )
 
