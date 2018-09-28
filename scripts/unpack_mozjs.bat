e534b24af52361685e90a8facc2c6b3644f6a50a
TheQwertiest <qwertiest@mail.ru>
2018-09-28 19:08:39 +0300

    Fixed mozjs script

--- scripts/unpack_mozjs.bat
+++ scripts/unpack_mozjs.bat
@@ -4,13 +4,16 @@ setlocal
 set PATH=%PATH%;C:\Program Files\7-Zip
 set ROOT_DIR=%~dp0..\
 
-set MOZ_JS_DIR=%ROOT_DIR%mozjs\
+set MOZ_JS_IN_DIR=%ROOT_DIR%submodules\mozjs\
+set MOZ_JS_OUT_DIR_NO_SLASH=%ROOT_DIR%mozjs
 
 echo Unpacking mozjs files
 
-for /d %%i in ("%MOZ_JS_DIR%"*) do rmdir /s /q "%%i"
-for /f "delims=" %%i in ('dir "%MOZ_JS_DIR%"* /b ^| findstr /r/i "mozjs.*\.zip"') do (
-    7z x -tzip -o"%MOZ_JS_DIR%" "%MOZ_JS_DIR%%%i" > NUL
+if exist "%MOZ_JS_OUT_DIR_NO_SLASH%" rmdir /s/q "%MOZ_JS_OUT_DIR_NO_SLASH%"
+mkdir "%MOZ_JS_OUT_DIR_NO_SLASH%"
+
+for /f "delims=" %%i in ('dir "%MOZ_JS_IN_DIR%"* /b ^| findstr /r/i "mozjs.*\.zip"') do (
+    7z x -tzip -o"%MOZ_JS_OUT_DIR_NO_SLASH%" "%MOZ_JS_IN_DIR%%%i" > NUL
     if errorlevel 1 goto fail
     goto success
 )
