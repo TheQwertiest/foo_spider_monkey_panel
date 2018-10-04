e5c8b4044211613e37e68d2ebd45e0f458762989
TheQwertiest <qwertiest@mail.ru>
2018-10-04 19:13:15 +0300

    Added a proper url to help from preferences page

--- foo_spider_monkey_panel/ui/ui_pref.h
+++ foo_spider_monkey_panel/ui/ui_pref.h
@@ -2,70 +2,81 @@
 
 #include "resource.h"
 
-class CDialogPref : public CDialogImpl<CDialogPref>, public CWinDataExchange<CDialogPref>, public preferences_page_instance
+class CDialogPref
+    : public CDialogImpl<CDialogPref>
+    , public CWinDataExchange<CDialogPref>
+    , public preferences_page_instance
 {
 public:
-	CDialogPref(preferences_page_callback::ptr callback) : m_callback(callback)
-	{
-	}
+    CDialogPref( preferences_page_callback::ptr callback )
+        : m_callback( callback )
+    {
+    }
 
-	BEGIN_DDX_MAP(CDialogPref)
-		DDX_CONTROL_HANDLE(IDC_LIST_EDITOR_PROP, m_props)
-	END_DDX_MAP()
+    BEGIN_DDX_MAP( CDialogPref )
+        DDX_CONTROL_HANDLE( IDC_LIST_EDITOR_PROP, m_props )
+    END_DDX_MAP()
 
-	BEGIN_MSG_MAP(CDialogPref)
-		MSG_WM_INITDIALOG(OnInitDialog)
-		COMMAND_HANDLER_EX(IDC_BUTTON_EXPORT, BN_CLICKED, OnButtonExportBnClicked)
-		COMMAND_HANDLER_EX(IDC_BUTTON_IMPORT, BN_CLICKED, OnButtonImportBnClicked)
-		NOTIFY_HANDLER_EX(IDC_LIST_EDITOR_PROP, NM_DBLCLK, OnPropNMDblClk)
-	END_MSG_MAP()
+    BEGIN_MSG_MAP( CDialogPref )
+        MSG_WM_INITDIALOG( OnInitDialog )
+        COMMAND_HANDLER_EX( IDC_BUTTON_EXPORT, BN_CLICKED, OnButtonExportBnClicked )
+        COMMAND_HANDLER_EX( IDC_BUTTON_IMPORT, BN_CLICKED, OnButtonImportBnClicked )
+        NOTIFY_HANDLER_EX( IDC_LIST_EDITOR_PROP, NM_DBLCLK, OnPropNMDblClk )
+    END_MSG_MAP()
 
-	BOOL OnInitDialog(HWND hwndFocus, LPARAM lParam);
+    BOOL OnInitDialog( HWND hwndFocus, LPARAM lParam );
 
-	enum
-	{
-		IDD = IDD_DIALOG_PREFERENCE
-	};
+    enum
+    {
+        IDD = IDD_DIALOG_PREFERENCE
+    };
 
-	HWND get_wnd();
-	LRESULT OnPropNMDblClk(LPNMHDR pnmh);
-	t_uint32 get_state();
-	void LoadProps(bool reset = false);
-	void OnButtonExportBnClicked(WORD wNotifyCode, WORD wID, HWND hWndCtl);
-	void OnButtonImportBnClicked(WORD wNotifyCode, WORD wID, HWND hWndCtl);
-	void OnChanged();
-	void OnEditChange(WORD, WORD, HWND);
-	void uGetItemText(int nItem, int nSubItem, pfc::string_base& out);
-	void apply();
-	void reset();
+    HWND get_wnd() override;
+    LRESULT OnPropNMDblClk( LPNMHDR pnmh );
+    t_uint32 get_state() override;
+    void LoadProps( bool reset = false );
+    void OnButtonExportBnClicked( WORD wNotifyCode, WORD wID, HWND hWndCtl );
+    void OnButtonImportBnClicked( WORD wNotifyCode, WORD wID, HWND hWndCtl );
+    void OnChanged();
+    void OnEditChange( WORD, WORD, HWND );
+    void uGetItemText( int nItem, int nSubItem, pfc::string_base& out );
+    void apply() override;
+    void reset() override;
 
 private:
-	CListViewCtrl m_props;
-	preferences_page_callback::ptr m_callback;
+    CListViewCtrl m_props;
+    preferences_page_callback::ptr m_callback;
 };
 
-class js_preferences_page_impl : public preferences_page_v3
+class js_preferences_page_impl 
+     : public preferences_page_v3
 {
 public:
-	const char* get_name()
-	{
-		return SMP_NAME;
-	}
+    const char* get_name() override
+    {
+        return SMP_NAME;
+    }
 
-	GUID get_guid()
-	{
-		return g_guid_smp_ui_pref;
-	}
+    GUID get_guid() override
+    {
+        return g_guid_smp_ui_pref;
+    }
 
-	GUID get_parent_guid()
-	{
-		return preferences_page::guid_tools;
-	}
+    GUID get_parent_guid() override
+    {
+        return preferences_page::guid_tools;
+    }
 
-	preferences_page_instance::ptr instantiate(HWND parent, preferences_page_callback::ptr callback)
-	{
-		service_impl_t<CDialogPref>* p = new service_impl_t<CDialogPref>(callback);
-		p->Create(parent);
-		return p;
-	}
+    bool get_help_url( pfc::string_base& p_out ) override
+    {
+        p_out = "https://github.com/TheQwertiest/foo_spider_monkey_panel/wiki";
+        return true;
+    }
+
+    preferences_page_instance::ptr instantiate( HWND parent, preferences_page_callback::ptr callback ) override
+    {
+        service_impl_t<CDialogPref>* p = new service_impl_t<CDialogPref>( callback );
+        p->Create( parent );
+        return p;
+    }
 };
