694f5206f5070ad8c32d950f3942208e0d35dfa2
TheQwertiest <qwertiest@mail.ru>
2018-10-14 15:32:55 +0300

    Docs cleanup

--- component/docs/js_doc/Interfaces.js
+++ component/docs/js_doc/Interfaces.js
@@ -1,4 +1,37 @@
 /**
+ * @constructor
+ * @param {string} name
+ * 
+ * @example
+ * var xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+ */
+function ActiveXObject(name) {
+
+    /**
+     * Emulates COM's weird behaviour of value accessors.
+     * 
+     * @return {*}
+     * 
+     * @example
+     * var saved_value = some_activex.ActiveX_Get(1, 'additional_info');
+     * // in COM:
+     * // var saved_value = some_activex.Item(1, 'additional_info');
+     */
+    this.ActiveX_Get = function () { };
+
+    /**
+     * Emulates COM's weird behaviour of value accessors.
+     *
+     * @example
+     * some_activex.ActiveX_Set(1, "new_value", 'additional_info');
+     * // in COM:
+     * // some_activex.Item(1, 'additional_info') = "new_value";
+     */
+    this.ActiveX_Set = function () { };
+}
+var xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+
+/**
  * Evaluates the script in file.
  * Similar to eval(utils.ReadTextFile(path)), but provides better error reporting
  *
