41549e4c3906767492c487043deb0a10b4bf0ee8
TheQwertiest <qwertiest@mail.ru>
2018-11-30 13:41:11 +0300

    [Docs] Added `fb.DoDragDrop` options

--- component/docs/js/foo_spider_monkey_panel.js
+++ component/docs/js/foo_spider_monkey_panel.js
@@ -332,6 +332,11 @@ let fb = {
      * @param {number} window_id see {@link window.ID}
      * @param {FbMetadbHandleList} handle_list
      * @param {number} effect Allowed effects.
+     * @param {object=} [options=undefined] Customization options for the data displayed in the drag window.
+     * @param {boolean=} [options.show_text=true] If true, will add track count text.
+     * @param {boolean=} [options.use_album_art=true] If true, will use album art of the focused item from dragged tracks (if available)
+     * @param {boolean=} [options.use_theming=true] If true, will use Windows drag window style. Album art and custom image are resized to fit when Windows style is active.
+     * @param {?GdiBitmap=} [options.custom_image=undefined] Custom image. Will be also displayed if use_album_art is true, but there is no album art available.
      * @return {number} Effect that was returned in {@link module:callbacks~on_drag_drop on_drag_drop}.
      *
      * @example
