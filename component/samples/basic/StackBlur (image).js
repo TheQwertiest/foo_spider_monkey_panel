db8a9888589987732f9808dc56b6a403139b474c
TheQwertiest <qwertiest@mail.ru>
2019-03-11 17:22:40 +0300

    [samples] Updated `StackBlur (image)` sample for a better appearance on smaller panels

--- component/samples/basic/StackBlur (image).js
+++ component/samples/basic/StackBlur (image).js
@@ -4,8 +4,11 @@ include(`${fb.ComponentPath}docs\\Helpers.js`);
 
 const img = gdi.Image(`${fb.ComponentPath}samples\\basic\\images\\post.jpg`);
 const font = gdi.Font('Segoe UI', 16, 1);
+const text_h = 24
 let blur_img = null;
 let radius = 20;
+let ww = 0;
+let wh = 0;
 
 StackBlur(radius);
 
@@ -15,12 +18,22 @@ function StackBlur(radius) {
 }
 
 function on_paint(gr) {
-    gr.DrawImage(blur_img, 0, 0, 600, 600, 0, 0, 600, 600);
-    gr.DrawImage(img, 6, 30, 200, 200, 0, 0, 600, 600);
+    if (!ww || !wh) {
+        return;
+    }
+
+    const min_dim = Math.min(ww,wh - text_h)
+    gr.DrawImage(blur_img, 0, text_h, min_dim, min_dim, 0, 0, 600, 600);
+    gr.DrawImage(img, 6, text_h + 5, min_dim/3, min_dim/3, 0, 0, 600, 600);
     // RGB function is defined in docs\\Helpers.js
     gr.FillSolidRect(0, 0, 600, 24, RGB(0, 0, 0));
     // DT_CENTER is defined in docs\\Flags.js
-    gr.GdiDrawText('Scroll mouse to change radius: ' + radius, font, RGB(255, 255, 255), 0, 0, 600, 24, DT_CENTER);
+    gr.GdiDrawText('Scroll mouse to change radius: ' + radius, font, RGB(255, 255, 255), 0, 0, ww, text_h, DT_CENTER);
+}
+
+function on_size(w,h) {
+    ww = w;
+    wh = h;
 }
 
 function on_mouse_wheel(step) {
