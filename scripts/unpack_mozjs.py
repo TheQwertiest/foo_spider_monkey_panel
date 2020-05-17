cb5893d1b609a59f6b83cb72d388e60cb573f9dc
TheQwertiest <qwertiest@mail.ru>
2020-05-17 15:12:50 +0300

    [scripts] Added timestamp preservation when unpacking mozjs

--- scripts/unpack_mozjs.py
+++ scripts/unpack_mozjs.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
+import os
+import time
 import zipfile
 from pathlib import Path
 from shutil import rmtree
 from zipfile import ZipFile
 
 import call_wrapper
+
+def restore_timestamps(zipname, extract_dir):
+    for f in zipfile.ZipFile(zipname, 'r').infolist():
+        fullpath = extract_dir/f.filename
+        # still need to adjust the dt o/w item will have the current dt
+        date_time = time.mktime(f.date_time + (0, 0, -1))
+        # update dt
+        os.utime(fullpath, (date_time, date_time))
 
 def unpack():
     cur_dir = Path(__file__).parent.absolute()
@@ -25,6 +35,8 @@ def unpack():
 
     with ZipFile(zip_arc) as z:
         z.extractall(output_dir)
+        
+    restore_timestamps(zip, output_dir)
 
 if __name__ == '__main__':
     call_wrapper.final_call_decorator(
