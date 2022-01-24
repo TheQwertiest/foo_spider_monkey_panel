bd5de314e4135fc454fcbaa73e4884645ddad0c9
Raziel Anarki <razielanarki@semmi.se>
2022-01-24 18:48:28 +0100

    cleanup hash_combiner.h, and use static inline constexpr-s

--- foo_spider_monkey_panel/utils/hash_combine.h
+++ foo_spider_monkey_panel/utils/hash_combine.h
@@ -1,47 +1,43 @@
 #pragma once
 
 #include <limits>
-#include <bit>
 
-// wether to use a custom hash combiner (with supposedly less collisions), or one based on boost's
+// wether to use a custom hash combiner (with less collisions)
+// or a relatively simpler, collision prone one based on boost's
 #define HASH_CUSTOM_COMBINER 1
 
 namespace smp::hash
 {
+#define HASHTYPE const std::size_t
+#define HASHEXPR static inline constexpr HASHTYPE
+#define HASHFUNC [[nodiscard]] HASHEXPR
 
 #if HASH_CUSTOM_COMBINER
-// mangled from:
-// https://newbedev.com/c-why-is-boost-hash-combine-the-best-way-to-combine-hash-values
-// (tldr: it's not)
-inline constexpr size_t h = std::numeric_limits<size_t>::digits / 2; // mulxor value shifted by (Half) bits
-inline constexpr size_t a = 0x55555555;                              // Alternating bits of 0101-s
-inline constexpr size_t r = 0x2e5bf271;                              // Random uneven int constant
-inline constexpr size_t t = std::numeric_limits<size_t>::digits / 3; // rotate seed by (Third) bits
-
-inline const size_t mxs( const size_t m, const size_t& x ) noexcept { return m * ( x ^ ( x >> h ) ); }
-inline const size_t dsp( const size_t& z ) noexcept { return mxs( r, mxs( a, z ) ); }
-
-inline const size_t combiner( const size_t& seed, const size_t& hash ) noexcept
-{
-    return std::rotl( seed, t ) ^ dsp( hash );
-}
+// use custom combiner (32/64bit agnostic)
+HASHEXPR d = std::numeric_limits<HASHTYPE>::digits;
+HASHEXPR i = std::numeric_limits<HASHTYPE>::digits10;
+// (compile time) helper recursive constexprs (pseudo Random Distributed Bits  + ALTernating bits)
+HASHFUNC rdb( HASHTYPE n ) noexcept { return 7 + ( n ? rdb( n - 1 ) * 10 : 0 ); }
+HASHFUNC alt( HASHTYPE n ) noexcept { return 5 + ( n ? alt( n - 1 ) << 4 : 0 ); }
+// custom combiner compositing helpers (Mul+Xor+Shift + ROTate)
+HASHFUNC mxs( HASHTYPE m, HASHTYPE x ) noexcept { return m * ( x ^ ( x >> ( d / 2 ) ) ); };
+HASHFUNC rot( HASHTYPE s, HASHTYPE b ) noexcept { return s << ( b % d ) | s >> ( d - b % d ); }
+// evenly distributing hash combiner
+HASHFUNC combiner( HASHTYPE seed, HASHTYPE hash ) noexcept { return rot( seed, d / 3 ) ^ mxs( rdb( i ), mxs( alt( d / 4 ), hash ) ); }
 #else
-inline const size_t combiner( const size_t& seed, const size_t& hash ) noexcept
-{
-    constexpr size_t boost_magic = 0x9e3779b9;
-    return ( seed ^ hash ) + boost_magic + ( seed << 6 ) + ( seed >> 2 );
-}
+// use boost magic (colliding) hash combiner
+HASHEVAL boost_magic = 0x9e3779b9;
+HASHFUNC combiner( HASHTYPE seed, HASHTYPE hash ) noexcept { return ( seed ^ hash ) + boost_magic + ( seed << 6 ) + ( seed >> 2 ); }
 #endif
 
-inline constexpr size_t combine( const std::size_t& seed ) noexcept
-{
-    return seed;
-}
+// var-args recursion end
+HASHFUNC combine( HASHTYPE seed ) noexcept { return seed; }
 
+// var-args template to recursively combine std::hash-es
 template <typename T, typename... Rest>
-inline constexpr size_t combine( const size_t& seed, const T& val, Rest... rest ) noexcept
+HASHFUNC combine( HASHTYPE seed, const T& val, Rest&&... rest ) noexcept
 {
-    return combine( combiner( seed, std::hash<T>{}( val ) ), rest... );
+    return hash::combine( hash::combiner( seed, std::hash<T>{}( val ) ), std::forward<Rest>( rest )... );
 };
 
 } // namespace
